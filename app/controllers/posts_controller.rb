0625d9a4c06047968854d9335efe4e18e07acd2b
Pálóczi Ádám <jatfield@gmail.com>
2016-09-07 09:25:11 +0200

    extra files removed, erroneous post redirects to its topic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
         format.html { redirect_to @topic }
         format.json { render :show, status: :created, location: @post }
       else
-        format.html { render :new }
+        format.html { redirect_to @topic }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end

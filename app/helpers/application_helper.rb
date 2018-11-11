6cea415729e9d6790735100b761c7532488c70a0
jatfield <jatfield@gmail.com>
2018-11-11 09:13:02 +0000

    day beforize

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,11 @@ module ApplicationHelper
   def day_beforize(time)
 
     return nil if !time
-    if time.day === Date.today.day
+    if time.to_date === Date.today
       "Ma, #{time.strftime('%H:%M')}"
-    elsif time.day === Date.today.advance(days: -1).day
+    elsif time.to_date === Date.today.advance(days: -1)
       "Tegnap, #{time.strftime('%H:%M')}"
-    elsif time.to_date.day === Date.today.advance(days: -2).day
+    elsif time.to_date === Date.today.advance(days: -2)
       "Tegnapelőtt, #{time.strftime('%H:%M')}"
     elsif time.year == Time.now.year
       l time, format: :topiclist

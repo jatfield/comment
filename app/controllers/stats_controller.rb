87575d9ddb6e0df48e4d9efb07c4ccd3372986b6
jatfield <jatfield@gmail.com>
2020-03-26 17:53:33 +0000

    tinymce height, stats months start/end

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,8 +1,8 @@
 class StatsController < ApplicationController
 
   def index
-    @from = params[:from] ? DateTime.parse(params[:from]) : DateTime.now.beginning_of_month
-    @to = params[:to] ? DateTime.parse(params[:to]) : DateTime.now.end_of_month
+    @from = params[:from] ? DateTime.parse(params[:from]).beginning_of_month : DateTime.now.beginning_of_month
+    @to = params[:to] ? DateTime.parse(params[:to]).end_of_month : DateTime.now.end_of_month
     @count = Post.joins(:user).where("posts.created_at BETWEEN ? AND ?", @from, @to).group(:username).count
   end
 end

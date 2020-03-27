b7634e50f19ead7d8c5aaa70cb383dd67200c39f
jatfield <jatfield@gmail.com>
2020-03-27 18:59:14 +0000

    added linechart

--- app/assets/javascripts/stats.js
+++ app/assets/javascripts/stats.js
@@ -1,34 +1,32 @@
 $(document).ready(function(){
-var container = document.getElementById('stats_chart');
-if (container) {
-var series_data = Object.entries(JSON.parse(container.dataset.series))
-console.log(series_data);
-var series = [];
+var barContainer = document.getElementById('stats_barchart');
+var lineContainer = document.getElementById('stats_linechart');
+if (barContainer) {
+var series_data = Object.entries(JSON.parse(barContainer.dataset.series))
+var line_series_data = Object.entries(JSON.parse(lineContainer.dataset.series))
+console.log(line_series_data);
+var barSeries = [];
 var max = 0;
 for (var i = 0; i < series_data.length; i++) {
 
-  series.push({name: series_data[i][0], data: [series_data[i][1]]})
+  barSeries.push({name: series_data[i][0], data: [series_data[i][1]]})
   max = series_data[i][1] > max ? series_data[i][1] : max
 }
-console.log(series);
+console.log(barSeries);
 var data = {
-    series: series
+    series: barSeries
     };
     var options = {
         chart: {
             width: 800,
             height: 650,
-            title: 'Hozzászólások',
             format: '1,000'
         },
         yAxis: {
-            title: 'User'
+            title: 'Hozzászólások'
         },
         xAxis: {
-            title: 'Hsz',
-            min: 0,
-            max: max,
-            suffix: 'db'
+            title: 'User',
         },
         series: {
             showLabel: true
@@ -52,9 +50,64 @@ var data = {
 
     // For apply theme
 
+    tui.chart.registerTheme('myTheme', theme);
+    options.theme = 'myTheme';
+
+    tui.chart.columnChart(barContainer, data, options);
+}
+if (lineContainer) {
+var line_series_data = Object.entries(JSON.parse(lineContainer.dataset.series))
+console.log(line_series_data);
+var lineSeries = [{name: 'Hozzászólások', data: []}];
+var max = 0;
+for (var i = 0; i < line_series_data.length; i++) {
+  lineSeries[0].data.push(line_series_data[i][1].post_count)
+}
+console.log(lineSeries);
+var data = {
+    categories: ['Január', 'Február', 'Március', 'Április', 'Május', 'Június', 'Július', 'Augusztus', 'Szeptember', 'Október', 'November', 'December'],
+    series: lineSeries
+    };
+    var options = {
+        chart: {
+            width: 800,
+            height: 650,
+        },
+        yAxis: {
+            title: 'Hozzászólások'
+        },
+        xAxis: {
+            title: 'Hónap',
+        },
+        series: {
+	    showDot: true,
+            showLabel: true
+        },
+	legend: {
+	    visible: false
+	},
+	tooltip: {
+	    suffix: 'db'
+	},
+	chartExportMenu: {
+	    visible: false
+	}
+    };
+    var theme = {
+        series: {
+            colors: [
+                '#882000', '#68d0a8', '#a838a0', '#50b818', '#181090',
+                '#f0e858', '#a04800', '#472b1b', '#c87870', '#484848',
+		'#808080', '#98ff98', '#5090d0', '#b8b8b8'
+            ]
+        }
+    };
+
+    // For apply theme
+
     // tui.chart.registerTheme('myTheme', theme);
     // options.theme = 'myTheme';
 
-    tui.chart.columnChart(container, data, options);
+    tui.chart.lineChart(lineContainer, data, options);
 }
 });
--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,7 +3,9 @@ class StatsController < ApplicationController
   def index
     @from = params[:from] ? DateTime.parse(params[:from]).beginning_of_month : DateTime.now.beginning_of_month
     @to = params[:to] ? DateTime.parse(params[:to]).end_of_month : DateTime.now.end_of_month
-    @count = Post.joins(:user).where("posts.created_at BETWEEN ? AND ?", @from, @to).group(:username).count
+    order = params[:order] == "user" ? 'username asc' : 'count_all desc'
+    @count = Post.joins(:user).where("posts.created_at BETWEEN ? AND ?", @from, @to).group(:username).order(order).count
     @sum_count = Post.where("posts.created_at BETWEEN ? AND ?", @from, @to).count
+    @post_count = ActiveRecord::Base.connection.execute("select date_part('month', created_at) as month, count(*) as post_count from posts where created_at between '#{@from.strftime("%Y") + '-01-01 00:00:01'}' and '#{@from.strftime("%Y") + '-12-31 23:59:59'}' group by month order by month")
   end
 end
